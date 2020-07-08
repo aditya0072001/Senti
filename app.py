b2c81f22d19c554fd43e0c2eb3dca4be9d11c8f4
Tripathi Aditya Prakash <41997646+aditya0072001@users.noreply.github.com>
2020-07-08 21:50:13 +0530

    making basic template for app

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask,redirect,url_for,render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("index.html",content="lol")
 
 
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!".format(name)
+    return render_template("index.html",content=name)
 
 if __name__ == '__main__':
     app.run()
